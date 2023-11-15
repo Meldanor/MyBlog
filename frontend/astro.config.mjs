c955ef7223affb0a1f048facb42a78548c3126d9
Kilian Gärtner <kilian.gaertner@i22.de>
2023-11-15 11:50:35 +0100

    add (frontend): compress build

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
-
+import Compress from 'astro-compress';
 import tailwind from '@astrojs/tailwind';
+
+import compressor from "astro-compressor";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), Compress(), compressor()]
 });
