68effd3d4e6a46fab4c44e39348a5000d9dd7009
Kilian Gärtner <kilian.gaertner@i22.de>
2023-12-23 22:47:33 +0100

    fix (frontend): deleting newlines in code blocks

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -2,9 +2,15 @@ import { defineConfig } from 'astro/config';
 import Compress from 'astro-compress';
 import tailwind from '@astrojs/tailwind';
 
-import compressor from "astro-compressor";
+import compressor from 'astro-compressor';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), Compress(), compressor()]
+  integrations: [
+    tailwind(),
+    Compress({
+      HTML: false
+    }),
+    compressor()
+  ]
 });
